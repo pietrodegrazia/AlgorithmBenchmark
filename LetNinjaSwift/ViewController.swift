299ed3999dcaa655f7864bce0a02238e120158da
pietrodegrazia <pietrodegrazia@hotmail.com>
2016-03-14 19:02:45 -0300

    Initial Commit

---
+++ LetNinjaSwift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LetNinjaSwift
+//
+//  Created by Pietro Degrazia on 3/14/16.
+//  Copyright © 2016 pietrodegrazia. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
