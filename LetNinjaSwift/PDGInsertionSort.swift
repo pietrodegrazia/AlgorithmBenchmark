6d14cea80f14b1077a8d390a37bdd10c7a126ade
pietrodegrazia <pietrodegrazia@hotmail.com>
2016-03-27 23:51:06 -0300

    Insertion Sort created

---
+++ LetNinjaSwift/PDGInsertionSort.swift
@@ -0,0 +1,16 @@
+//
+//  PDGInsertionSort.swift
+//  LetNinjaSwift
+//
+//  Created by Pietro Degrazia on 3/27/16.
+//  Copyright © 2016 pietrodegrazia. All rights reserved.
+//
+
+import Foundation
+
+class PDGInsertionSort: PDGAlgorithm {
+    func sort() {
+        print("sort called for insertion sort")
+    }
+}
+
